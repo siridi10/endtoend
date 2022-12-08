94ec140dd2afd654028cb45cd078b6edb52955ef
siridi10 <siridirock@gmail.com>
2022-12-08 20:05:53 +0530

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting a New new new Project"
+    return "Started CI/CD PIPELINE Project"
 
 if __name__=='__main__':
     app.run(debug=True)
